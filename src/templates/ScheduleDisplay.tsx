c7626d5b20f3d6d97b3609a16af4276d2fdc8d24
Dana Gurland <danagurland@danas-mbp.mynetworksettings.com>
2024-04-21 21:53:21 -0400

    first draft of scheduledisplay by activity area

--- src/templates/ScheduleDisplay.tsx
+++ src/templates/ScheduleDisplay.tsx
@@ -16,6 +16,7 @@ import { organizeTableEntries } from "../common/helpers";
 import { MenuItem, Select } from "@mui/material";
 import dayjs from "dayjs";
 import classNames from "classnames";
+import { Switch } from '@mui/material';
 
 type ScheduleDisplayProps = {
   schedule?: Schedule;
@@ -25,12 +26,18 @@ type ScheduleDisplayProps = {
 const ScheduleDisplay = (props: ScheduleDisplayProps) => {
   const { defaultDivision } = props;
   const [scheduleEntriesByPeriod, setScheduleEntries] = useState<ScheduleEntry[][]>([]);
-  const numPeriods = 6;
+  const numPeriods = 6;//TODO
+  const userActivities = ["2914ee2b-8592-4b27-ad03-3d2d179c8222", "87a27617-4b65-4a6f-9895-7ee7909163ed", "7f80bc20-2da6-431f-b565-84378e9f9136"]//TODO
+  const [filterFormat, setFilterFormat] = useState(false);
   const API = generateClient({ authMode: 'apiKey' });
   const [divisionForMobile, setDivisionForMobile] = useState<DIVISIONS | undefined>(defaultDivision ?? DIVISIONS.JRG);
   useEffect(() => {
     setScheduleEntries(organizeTableEntries(props.schedule?.entries?.items));
   }, [props.schedule]);
+
+  useEffect(() => {
+    console.log('hey')
+  }, [filterFormat])
 
   useEffect(() => {
     setDivisionForMobile(defaultDivision != undefined ? defaultDivision : DIVISIONS.JRG);
@@ -42,19 +49,35 @@ const ScheduleDisplay = (props: ScheduleDisplayProps) => {
     for (let i = 0; i < numPeriods; i++) {
       const row = [];
       const dataForPeriod: ScheduleEntry[] = scheduleEntriesByPeriod[i] ?? [];
-      for (let j = 0; j < divisions.length; j++) {
-        let divisionData = dataForPeriod.find((dataEntry) => dataEntry.division == j);
-        if (divisionData) {
-          row[j] = divisionData;
-        } else {
-          row[j] = {
-            id: "",
-            activities: [],
-            period: i,
-            division: j
-          };
+      if (!filterFormat) {
+        for (let j = 0; j < divisions.length; j++) {
+          let divisionData = dataForPeriod.find((dataEntry) => dataEntry.division == j);
+          if (divisionData) {
+            row[j] = divisionData;
+          } else {
+            row[j] = {
+              id: "",
+              activities: [],
+              period: i,
+              division: j
+            };
+          }
+        }
+      } else {
+        const divisionsToInclude = []
+        dataForPeriod.forEach((dataEntry) => {
+          dataEntry?.activities?.items?.forEach((item) => {
+            if (item.activity?.id && userActivities.includes(item.activity.id)) {
+              divisionsToInclude.push(`${DIVISIONS[dataEntry.division]}`)
+            }
+          })
+        })
+        row[0] = {
+          period: i,
+          activities: divisionsToInclude
         }
       }
+
       rows.push(row)
     }
     return rows;
@@ -68,7 +91,7 @@ const ScheduleDisplay = (props: ScheduleDisplayProps) => {
 
   return (
     <div>
-      <div className="sm:hidden w-full my-2 px-4 container">
+      {!filterFormat && (<div className="sm:hidden w-full my-2 px-4 container">
         <Select value={divisionForMobile}
           onChange={(event) => setDivisionForMobile(event?.target.value as DIVISIONS)}
           classes={{
@@ -82,7 +105,7 @@ const ScheduleDisplay = (props: ScheduleDisplayProps) => {
           })}
           <MenuItem value={undefined} key={"all"}>Show all</MenuItem>
         </Select>
-      </div>
+      </div>)}
       <div className="m-4 font-bold text-xl">
         {dayjs().isSame(dayjs(props.schedule?.date, "MM/DD/YYYY"), 'day') ? <span>Today's Schedule</span> : <span>Schedule For {props.schedule?.date}</span>}
       </div>
@@ -91,11 +114,11 @@ const ScheduleDisplay = (props: ScheduleDisplayProps) => {
           <TableHead>
             <TableRow>
               <TableCell />
-              {Object.keys(DIVISIONS).filter((key) => isNaN(Number(key))).map((divisionKey) => {
+              {!filterFormat ? Object.keys(DIVISIONS).filter((key) => isNaN(Number(key))).map((divisionKey) => {
                 return (
-                  <TableCell key={divisionKey} className={classNames("sm:flex", { "hidden": divisionForMobile !== undefined &&  divisionForMobile !== DIVISIONS[divisionKey] })}>{divisionKey}</TableCell>
+                  <TableCell key={divisionKey} className={classNames("sm:flex", { "hidden": divisionForMobile !== undefined && divisionForMobile !== DIVISIONS[divisionKey] })}>{divisionKey}</TableCell>
                 )
-              })}
+              }) : <TableCell />}
             </TableRow>
           </TableHead>
           <TableBody>
@@ -106,17 +129,29 @@ const ScheduleDisplay = (props: ScheduleDisplayProps) => {
                     Period {i + 1}
                   </TableCell>
                   {row.map((entry: ScheduleEntry, j) => {
-                    return (
-                      <TableCell key={`${i}-${j}`} className={classNames("sm:flex", { "hidden": divisionForMobile !== undefined && divisionForMobile !== j })}>
-                        {entry.activities?.items?.map((activity) => `${activity.activity.name}${activity.label ? " (" + activity.label + ")" : ''}`).join(', ')}
-                      </TableCell>
-                    )
+                    if (!filterFormat) {
+                      return (
+                        <TableCell key={`${i}-${j}`} className={classNames("sm:flex", { "hidden": divisionForMobile !== undefined && divisionForMobile !== j })}>
+                          {entry.activities?.items?.map((activity) => `${activity.activity.name}${activity.label ? " (" + activity.label + ")" : ''}`).join(', ')}
+                        </TableCell>
+                      )
+                    } else {
+                      return (
+                        <TableCell key={`${i}-${j}`} className={classNames("sm:flex")}>
+                          {entry.activities.join(', ')}
+                        </TableCell>
+                      )
+                    }
+ 
                   })}
                 </TableRow>
               )
             })}
           </TableBody>
         </Table>
+      </div>
+      <div className="absolute bottom-0 right-2 z-[2]">
+        by division<Switch checked={filterFormat} onChange={(event) => setFilterFormat(event.target.checked)}></Switch>by activity area
       </div>
     </div>
   );
